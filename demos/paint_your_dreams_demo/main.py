d6127bbd85638ff675be224df6ec69483174f9e5
Adrian Boguszewski <adrian.boguszewski@intel.com>
2024-07-04 15:44:56 +0200

    Added seed management for paint your dreams

--- demos/paint_your_dreams_demo/main.py
+++ demos/paint_your_dreams_demo/main.py
@@ -1,10 +1,12 @@
 import argparse
 import os
+import random
 import sys
 import time
 
 import gradio as gr
 import numpy as np
+import torch
 from diffusers.pipelines.stable_diffusion import StableDiffusionSafetyChecker
 from optimum.intel.openvino import OVLatentConsistencyModelPipeline
 
@@ -18,14 +20,24 @@ ov_pipeline: OVLatentConsistencyModelPipeline | None = None
 safety_checker: StableDiffusionSafetyChecker | None = None
 
 
+MAX_SEED = np.iinfo(np.int32).max
+
+
 def load_pipeline(model_name, device):
     global ov_pipeline, safety_checker
 
+    ov_config = {"CACHE_DIR": "cache"}
     safety_checker = StableDiffusionSafetyChecker.from_pretrained("CompVis/stable-diffusion-safety-checker")
-    ov_pipeline = OVLatentConsistencyModelPipeline.from_pretrained(model_name, compile=False, safety_checker=safety_checker)
+    ov_pipeline = OVLatentConsistencyModelPipeline.from_pretrained(model_name, compile=False, safety_checker=safety_checker, ov_config=ov_config)
 
     ov_pipeline.to(device)
     ov_pipeline.compile()
+
+
+def randomize_seed_fn(seed: int, randomize_seed: bool = True) -> int:
+    if randomize_seed:
+        seed = random.randint(0, MAX_SEED)
+    return seed
 
 
 def stop():
@@ -33,7 +45,7 @@ def stop():
     stop_generating = True
 
 
-def generate_images(prompt: str, size: int, guidance_scale: float, num_inference_steps: int):
+def generate_images(prompt: str, seed: int, size: int, guidance_scale: float, num_inference_steps: int, randomize_seed: bool):
     global stop_generating
 
     stop_generating = False
@@ -41,8 +53,13 @@ def generate_images(prompt: str, size: int, guidance_scale: float, num_inference
         if stop_generating:
             break
 
+        seed = randomize_seed_fn(seed, randomize_seed)
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
         start_time = time.time()
-        result = ov_pipeline(prompt=prompt, num_inference_steps=num_inference_steps, width=size, height=size, guidance_scale=guidance_scale).images
+        result = ov_pipeline(prompt=prompt, num_inference_steps=num_inference_steps, width=size, height=size,
+                             guidance_scale=guidance_scale).images
         end_time = time.time()
 
         result, nsfw = safety_checker(ov_pipeline.feature_extractor(result, return_tensors="pt").pixel_values, np.array(result))
@@ -50,7 +67,7 @@ def generate_images(prompt: str, size: int, guidance_scale: float, num_inference
         utils.draw_ov_watermark(result[0], size=0.60)
 
         processing_time = end_time - start_time
-        yield result[0], round(processing_time, 5)
+        yield result[0], round(processing_time, 5), seed
 
 
 def build_ui():
@@ -82,6 +99,11 @@ def build_ui():
                 gr.Column(scale=1)
             with gr.Accordion("Advanced options", open=False):
                 with gr.Row():
+                    seed = gr.Slider(label="Seed", minimum=0, maximum=MAX_SEED, step=1, value=0, randomize=True,
+                                     scale=1)
+                    randomize_seed = gr.Checkbox(label="Randomize seed across runs", value=True, scale=0)
+                    randomize_seed_button = gr.Button("Randomize seed", scale=0)
+                with gr.Row():
                     guidance_scale = gr.Slider(
                         label="Guidance scale for base",
                         minimum=2,
@@ -112,16 +134,22 @@ def build_ui():
             cache_examples=False,
         )
         gr.on(
+            triggers=[randomize_seed_button.click],
+            fn=randomize_seed_fn,
+            inputs=[seed],
+            outputs=[seed],
+        )
+        gr.on(
             triggers=[
                 prompt.submit,
                 run_button.click,
             ],
             fn=generate_images,
             inputs=[
-                prompt, size, guidance_scale, num_inference_steps
+                prompt, seed, size, guidance_scale, num_inference_steps, randomize_seed
             ],
             outputs=[
-                result, result_time_label
+                result, result_time_label, seed
             ],
         )
         run_button.click(lambda: gr.Button(interactive=False), outputs=run_button) \
@@ -137,7 +165,7 @@ def run_endless_lcm(model_name, device):
     load_pipeline(model_name, device)
 
     demo = build_ui()
-    demo.launch()
+    demo.launch(server_name="0.0.0.0")
 
 
 if __name__ == '__main__':
