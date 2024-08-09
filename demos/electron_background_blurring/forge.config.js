b5ef98aa0cea85362939a25916f0c2f1d3f92b10
Adrian Boguszewski <adrian.boguszewski@intel.com>
2024-08-09 14:36:35 +0200

    Added icon

--- demos/electron_background_blurring/forge.config.js
+++ demos/electron_background_blurring/forge.config.js
@@ -4,6 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    icon: "assets/icons/icon",
   },
   rebuildConfig: {},
   makers: [
@@ -17,11 +18,19 @@ module.exports = {
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          icon: 'assets/icons/icon.png'
+        }
+      }
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        options: {
+          icon: 'assets/icons/icon.png'
+        }
+      }
     },
   ],
   plugins: [
--- demos/electron_background_blurring/src/main.js
+++ demos/electron_background_blurring/src/main.js
@@ -7,6 +7,7 @@ function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, 'assets', 'icons', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
