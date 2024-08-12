f8a23dc0704e61ab11d422711c63c7958e9b38e1
Adrian Boguszewski <adrian.boguszewski@intel.com>
2024-08-12 17:39:43 +0200

    Added cache for the OV models

--- demos/electron_background_blurring/src/ov-jobs.js
+++ demos/electron_background_blurring/src/ov-jobs.js
@@ -5,11 +5,14 @@ const { performance } = require('perf_hooks');
 module.exports = { detectDevices, runModel }
 
 const core = new ov.Core();
+const ovModels = new Map()
 
 async function detectDevices() {
     return ["AUTO"].concat(core.getAvailableDevices());
 }
 
 async function runModel(img, device){
+    // if device in ovModels, use precompiled model, otherwise load and compile model and ut to the map
+
     return img;
 }
