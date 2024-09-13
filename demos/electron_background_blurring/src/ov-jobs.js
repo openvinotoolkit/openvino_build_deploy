dadcda6f1778e6609573186256bf097706fbb3bc
roszczyk <mikolaj.roszczyk@intel.com>
2024-09-13 14:38:42 +0200

    blurring changed to model

--- demos/electron_background_blurring/src/ov-jobs.js
+++ demos/electron_background_blurring/src/ov-jobs.js
@@ -3,7 +3,6 @@ const { cv } = require('opencv-wasm');
 const { performance } = require('perf_hooks');
 const path = require('path');
 const fs = require('fs');
-const StackBlur = require( 'stackblur-canvas' );
 
 module.exports = { detectDevices, runModel, takeTime, blurImage }
 
@@ -162,15 +161,8 @@ async function runModel(img, width, height, device){
         console.log(performance.now()-begin, "postprocessing");
 
         // MASK PREPARATION
-        cv.threshold(maskMatOrg, maskMatOrg, 0, 255, cv.THRESH_BINARY);
+        cv.threshold(maskMatOrg, maskMatOrg, 0, 1, cv.THRESH_BINARY);
         console.log(performance.now()-begin, "threshold");
-
-        if (notMask == null || notMask.data.length !== maskMatOrg.data.length){
-            notMask = new cv.Mat(height, width, cv.CV_8UC1);
-        }
-        cv.bitwise_not(maskMatOrg, notMask);
-        cv.threshold(notMask, notMask, 254, 255, cv.THRESH_BINARY);
-        console.log(performance.now()-begin, "not mask declared");
 
         countRun++;
         console.log("\nmodel run:", countRun,"time:", performance.now()-begin, "\n");
@@ -188,38 +180,49 @@ async function runModel(img, width, height, device){
 
 let matToBlur = null;
 let smallImage = null;
+let blurModelRead = null;
+let blurModelCompiled = null;
 
 async function blurImage(image, width, height){
     const begin = performance.now();
+    let inferRequestBlur;
     
     if (matToBlur == null || matToBlur.data.length !== image.data.length){
         matToBlur = new cv.Mat(height, width, cv.CV_8UC4);
     }
     matToBlur.data.set(image.data);
 
-    if (blurredImage == null || matToBlur.data.length !== blurredImage.data.length){
-        blurredImage = new cv.Mat(height, width, cv.CV_8UC4);
+    if (blurModelCompiled == null){
+        if (fs.existsSync(path.join(__dirname, '../../app.asar'))){
+            blurModelRead = await core.readModel(path.join(__dirname, "../../app.asar.unpacked/models/spostproc_model.xml"));
+        } else {
+        blurModelRead = await core.readModel(path.join(__dirname, "../models/postproc_model.xml"));
+        }
+        blurModelCompiled = await core.compileModel(blurModelRead, "AUTO");
     }
+
+    // MASK TENSOR
+    const tensorDataMask = Float32Array.from(maskMatOrg.data);
+    const shapeMask = [1, maskMatOrg.rows, maskMatOrg.cols, 1];
+    const inputTensorMask = new ov.Tensor(ov.element.f32, shapeMask, tensorDataMask);
+
+    // IMAGE TENSOR
+    const tensorDataImage = Float32Array.from(image.data, x => x / 255.0);
+    const shapeImage = [1, maskMatOrg.rows, maskMatOrg.cols, 3];
+    const inputTensorImage = new ov.Tensor(ov.element.f32, shapeImage, tensorDataImage);
     
-    blurredImage.data.set(image.data);
-    cv.blur(matToBlur, blurredImage, new cv.Size(25,25));
+    // INFERENCE
+    inferRequestBlur = compiledModel.createInferRequest();
+    inferRequestBlur.setInputTensor(blurModelCompiled.inputs[0], inputTensorImage); // Ustaw pierwszy tensor wejściowy
+    inferRequestBlur.setInputTensor(blurModelCompiled.inputs[1], inputTensorMask);
+    inferRequestBlur.infer();
+    const outputLayer = compiledModel.outputs[0];
+    const resultInfer = inferRequestBlur.getTensor(outputLayer);
 
     if (finalMat == null || matToBlur.data.length !== finalMat.data.length){
         finalMat = new cv.Mat(height, width, cv.CV_8UC4);
     }
-    // console.log(performance.now()-begin, "final mat declared");
-
-    if (alpha == null || matToBlur.data.length !== alpha.data.length) {
-        alpha = new cv.Mat(height, width, matToBlur.type(), new cv.Scalar(0, 0, 0, 0)); 
-    }
-
-    cv.bitwise_and(matToBlur, alpha, matToBlur, mask=notMask);
-    // console.log(performance.now()-begin, "AND org");
-    cv.bitwise_and(blurredImage, alpha, blurredImage, mask=maskMatOrg);
-    // console.log(performance.now()-begin, "AND blur");
-
-    cv.add(matToBlur, blurredImage, finalMat);
-    // console.log(performance.now()-begin, "ADD final");
+    finalMat.data.set(resultInfer);
 
     countBlur++;
     console.log("\nmodel blur:", countBlur,"time:", performance.now()-begin, "\n");
