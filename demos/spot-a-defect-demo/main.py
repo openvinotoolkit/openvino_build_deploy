53a1e968a0c33cbd6cd640a4f0a2cba4edd4ed5f
Adrian Boguszewski <adrian.boguszewski@intel.com>
2025-02-05 16:51:36 +0100

    Removed anomalib

--- demos/spot-a-defect-demo/main.py
+++ demos/spot-a-defect-demo/main.py
@@ -7,17 +7,10 @@ from pathlib import Path
 
 import cv2
 import numpy as np
-from anomalib.data import MVTec, NumpyImageBatch
-from anomalib.deploy import ExportType, OpenVINOInferencer
-from anomalib.engine import Engine
-from anomalib.metrics import F1Max, Evaluator
-from anomalib.models import get_model
-from lightning.pytorch.callbacks import EarlyStopping
 from supervision import BoxCornerAnnotator, LabelAnnotator, TraceAnnotator, LineZoneAnnotator, LineZone, ByteTrack, \
     Point, Detections, Color, ColorLookup, \
     DetectionsSmoother
 from supervision.annotators.base import BaseAnnotator
-from torchvision.transforms import v2
 from ultralytics import YOLOWorld, YOLO
 from ultralytics.engine.results import Results
 from ultralytics.models.yolo.detect import DetectionPredictor
@@ -56,41 +49,6 @@ def load_yolo_model(model_name: str) -> YOLOWorld:
     # model.predictor = predictor
 
     return model
-
-
-def train_anomalib_model(model_name: str) -> Path:
-    # augmentation are needed for mitigating domain shift
-    augmentations = v2.Compose([
-        v2.RandomHorizontalFlip(p=0.5),  # Randomly flip images horizontally with 50% probability
-        v2.RandomVerticalFlip(p=0.2),  # Randomly flip images vertically with 20% probability
-        v2.RandomRotation(degrees=30),  # Randomly rotate images within a range of ±30 degrees
-        v2.RandomResizedCrop(size=(224, 224), scale=(0.8, 1.0)),  # Randomly crop and resize images
-        v2.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.2),  # Randomly adjust colors
-        v2.RandomGrayscale(p=0.1),  # Convert images to grayscale with 10% probability
-    ])
-
-    val_metrics = [F1Max(fields=["pred_score", "gt_label"])]
-    evaluator = Evaluator(val_metrics=val_metrics, test_metrics=val_metrics)
-    early_stopping = EarlyStopping(monitor="F1Max", patience=3, mode="max")
-
-    # define the class to train on
-    datamodule = MVTec(DATA_DIR / "mvtec", category="hazelnut", augmentations=augmentations)
-    model = get_model(model_name)
-    model.evaluator = evaluator
-
-    # validation and testing are not needed for this demo
-    engine = Engine(max_epochs=10, limit_test_batches=0, callbacks=[early_stopping], accelerator="cpu")
-    engine.fit(datamodule=datamodule, model=model)
-    # export model to openvino
-    return engine.export(model, ExportType.OPENVINO, MODEL_DIR / model_name, input_size=(256, 256))
-
-
-def load_anomalib_model(model_name: str) -> OpenVINOInferencer:
-    model_path = MODEL_DIR / model_name / "weights" / "openvino" / "model.xml"
-    if not model_path.exists():
-        model_path = train_anomalib_model(model_name)
-
-    return OpenVINOInferencer(model_path)
 
 
 def load_annotators(size: tuple[int, int]) -> tuple[list[BaseAnnotator], LineZone, ByteTrack, DetectionsSmoother]:
@@ -161,42 +119,16 @@ def get_patches(frame: np.ndarray, results: Detections) -> np.ndarray:
     return np.array(patches)
 
 
-def draw_results(frame: np.ndarray, annotators: list[BaseAnnotator], line_zone: LineZone, detections: Detections, anomaly_results: NumpyImageBatch, visualize: str) -> None:
+def draw_results(frame: np.ndarray, annotators: list[BaseAnnotator], line_zone: LineZone, detections: Detections) -> None:
     for annotator in annotators:
         if isinstance(annotator, LineZoneAnnotator):
             annotator.annotate(frame, line_counter=line_zone)
         else:
             annotator.annotate(frame, detections)
 
-    # todo anomaly visualization
-    # for box, anomaly in zip(detections.xyxy, anomaly_results):
-    #     x1, y1, x2, y2 = add_box_margin(box, frame.shape[:2][::-1])
-    #     anomaly_score = float(anomaly.pred_score)
-    #
-    #     # draw anomaly map
-    #     if visualize == "heatmaps":
-    #         anomaly_map = cv2.normalize(anomaly.anomaly_map, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-    #         colormap = cv2.applyColorMap(anomaly_map.astype(np.uint8), cv2.COLORMAP_JET)
-    #         colormap = cv2.resize(colormap, (x2 - x1, y2 - y1))
-    #         frame[y1:y2, x1:x2] = cv2.addWeighted(frame[y1:y2, x1:x2], 0.6, colormap, 0.4, 0)
-    #     # draw anomaly mask
-    #     elif visualize == "masks":
-    #         pred_mask = anomaly.pred_mask.astype(np.uint8) * 255
-    #         mask = np.zeros_like(pred_mask)
-    #         mask = np.dstack((mask, mask, pred_mask))
-    #         mask = cv2.resize(mask, (x2 - x1, y2 - y1))
-    #         frame[y1:y2, x1:x2] = cv2.addWeighted(frame[y1:y2, x1:x2], 0.6, mask, 0.4, 0)
-    #
-    #     color = (0, 0, 255) if anomaly_score > 0.5 else (0, 255, 0)
-    #     # draw a red rectangle around the object
-    #     cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
-    #     # draw anomaly score
-    #     utils.draw_text(frame, text=f"Anomaly: {anomaly_score:.2f}", point=(int(x1), int(y1)))
 
-
-def run(video_path: str, det_model_name: str, anomaly_model_name: str, visualize: str, flip: bool):
+def run(video_path: str, det_model_name: str, flip: bool):
     det_model = load_yolo_model(det_model_name)
-    anomaly_model = load_anomalib_model(anomaly_model_name)
 
     video_size = (1920, 1080)
     # initialize video player to deliver frames
@@ -227,12 +159,9 @@ def run(video_path: str, det_model_name: str, anomaly_model_name: str, visualize
         det_results = det_model.predict(frame, conf=0.01, verbose=False)[0]
         det_results = filter_and_process_results(det_results, tracker, smoother, line_zone)
 
-        patches = get_patches(frame, det_results)
-        anomalies = anomaly_model.predict(patches) if len(patches) > 0 else NumpyImageBatch(frame)
-
         end_time = time.time()
 
-        draw_results(frame, annotators, line_zone, det_results, anomalies, visualize)
+        draw_results(frame, annotators, line_zone, det_results)
 
         processing_times.append(end_time - start_time)
         # mean processing time [ms]
@@ -260,10 +189,7 @@ if __name__ == '__main__':
     parser.add_argument("--detection_model", type=str, default="yolov8s-worldv2", help="Model for object detection",
                         choices=["yolov8s-world", "yolov8m-world", "yolov8l-world", "yolov8x-world",
                                  "yolov8s-worldv2", "yolov8m-worldv2", "yolov8l-worldv2", "yolov8x-worldv2"])
-    parser.add_argument("--anomaly_model", type=str, default="Stfpm", help="Model for anomaly detection")
-    parser.add_argument("--visualize", type=str, default=None, help="Visualization type",
-                        choices=["heatmaps", "masks"])
     parser.add_argument("--flip", type=bool, default=True, help="Mirror input video")
 
     args = parser.parse_args()
-    run(args.stream, args.detection_model, args.anomaly_model, args.visualize, args.flip)
+    run(args.stream, args.detection_model, args.flip)
