e0a99f9dbfe26642820ba0a7f63c4e38de3659ce
Adrian Boguszewski <adrian.boguszewski@intel.com>
2025-03-27 13:18:21 +0100

    Added CI testing file for custom AI assistant

--- ai_ref_kits/custom_ai_assistant/ci/test.py
+++ ai_ref_kits/custom_ai_assistant/ci/test.py
@@ -13,17 +13,12 @@ if __name__ == '__main__':
     model_dir = Path("model")
     asr_model_type = "distil-whisper-large-v3"
     asr_precision = "fp16"
-    embedding_model_type = "bge-small"
-    reranker_model_type = "bge-reranker-base"
     chat_model_type = "llama3.2-1B"
     chat_precision = "int4"
     personality_path = Path("../config/concierge_personality.yaml")
     example_pdf = Path("../data/Grand_Azure_Resort_Spa_Full_Guide.pdf")
 
     asr_model_dir = asr.convert_asr_model(asr_model_type, asr_precision, model_dir)
-
-    embedding_model_dir = chat.convert_embedding_model(embedding_model_type, model_dir)
-    reranker_model_dir = chat.convert_reranker_model(reranker_model_type, model_dir)
     chat_model_dir = chat.convert_chat_model(chat_model_type, chat_precision, model_dir)
 
-    app.run(asr_model_dir, chat_model_dir, embedding_model_dir, reranker_model_dir, personality_path, example_pdf)
+    app.run(asr_model_dir, chat_model_dir)
