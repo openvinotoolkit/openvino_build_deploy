6ceb0b807655f77e27d7b8a1d66738579184db67
Zhuo Wu <zhuo.wu@intel.com>
2025-07-24 13:45:17 +0800

    Update main.py

--- demos/people_counter_demo/main.py
+++ demos/people_counter_demo/main.py
@@ -1,23 +1,24 @@
-
 import argparse
-from collections import deque
-from typing import List
-import sys, os
-import cv2
 import json
-import numpy as np
-import logging
+import logging as log
+import os
+import sys
 import time
-from ultralytics import YOLO
+from collections import defaultdict, deque
+from pathlib import Path
+from typing import Tuple, List, Dict
 
+import cv2
+import numpy as np
+import supervision as sv
+from supervision import ColorLookup
+from ultralytics import YOLO
 
 SCRIPT_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "utils")
 sys.path.append(os.path.dirname(SCRIPT_DIR))
-from utils import demo_utils as utils
 
-# Suppress Ultralytics verbose logs
-logging.getLogger("ultralytics").setLevel(logging.ERROR)
-logging.basicConfig(level=logging.INFO)
+from utils import demo_utils as utils
+from deep_sort_realtime.deepsort_tracker import DeepSort
 
 CATEGORIES = [
     "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
@@ -30,6 +31,27 @@ CATEGORIES = [
     "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
 ]
 
+def convert(model_name: str, model_dir: Path) -> tuple[Path, Path]:
+    model_path = f"{model_name}.pt"
+    # create a YOLO object detection model
+    yolo_model = YOLO(model_path)
+
+    ov_model_path = model_dir / f"{model_name}_openvino_model"
+    ov_int8_model_path = model_dir / f"{model_name}_int8_openvino_model"
+    # export the model to OpenVINO format (FP16 and INT8)
+    if not ov_model_path.exists():
+        ov_model_path = yolo_model.export(format="openvino", dynamic=False, half=True)
+    if not ov_int8_model_path.exists():
+        ov_int8_model_path = yolo_model.export(format="openvino", dynamic=False, half=True, int8=True, data="coco128.yaml")
+    return Path(ov_model_path), Path(ov_int8_model_path)
+
+
+def get_model(model_path: Path, verbose: bool = False):
+    # compile the model with YOLO
+    model = YOLO(model_path, verbose=False)
+    return model
+
+
 def load_zones(json_path: str) -> List[np.ndarray]:
     # load json file
     with open(json_path) as f:
@@ -39,47 +61,128 @@ def load_zones(json_path: str) -> List[np.ndarray]:
     return [np.array(zone["points"], np.int32) for zone in zones_dict.values()]
 
 
-def run(video_path: str, model_name: str = "", category: str = "person", zones_config_file: str = "",
+def get_annotators(json_path: str, resolution_wh: Tuple[int, int], colorful: bool = False) -> Tuple[List, List, List, List, List]:
+    # list of points
+    polygons = load_zones(json_path)
+
+    # colors for zones
+    colors = sv.ColorPalette.DEFAULT
+
+    zones = []
+    zone_annotators = []
+    box_annotators = []
+    masks_annotators = []
+    label_annotators = []
+    for index, polygon in enumerate(polygons, start=1):
+        # a zone to count objects in
+        zone = sv.PolygonZone(polygon=polygon, frame_resolution_wh=resolution_wh)
+        zones.append(zone)
+        # the annotator - visual part of the zone
+        zone_annotators.append(sv.PolygonZoneAnnotator(zone=zone, color=colors.by_idx(index), thickness=0))
+        # box annotator, showing boxes around objects
+        box_annotator = sv.BoxAnnotator(color_lookup=ColorLookup.INDEX) if colorful else sv.BoxAnnotator(color=colors.by_idx(index))
+        box_annotators.append(box_annotator)
+        # mask annotator, showing transparent mask
+        mask_annotator = sv.MaskAnnotator(color_lookup=ColorLookup.INDEX) if colorful else sv.MaskAnnotator(color=colors.by_idx(index))
+        masks_annotators.append(mask_annotator)
+        # label annotator, showing people ids
+        label_annotator = sv.LabelAnnotator(text_scale=0.7, color_lookup=ColorLookup.INDEX) if colorful else sv.LabelAnnotator(text_scale=0.7, color=colors.by_idx(index))
+        label_annotators.append(label_annotator)
+
+    return zones, zone_annotators, box_annotators, masks_annotators, label_annotators
+
+
+def track_objects(frame: np.array, detections: sv.Detections, tracker: DeepSort) -> List:
+    # Convert detections to the format required by the tracker
+    detection_list = []
+    for det in zip(detections.xyxy, detections.confidence):
+        bbox = det[0].tolist()
+        confidence = det[1]
+        detection_list.append((bbox, confidence))
+ 
+    # Update the tracker with the new detections
+    tracks = tracker.update_tracks(detection_list, frame=frame)
+    # detections are sorted by confidence so tracks must be also sorted the same way
+    return list(sorted(tracks, key=lambda x: x.det_conf if x.det_conf is not None else 0.0, reverse=True))
+
+
+def draw_annotations(frame: np.array, detections: sv.Detections, tracker: DeepSort, queue_count: Dict, object_limit: int, category:str,
+                     zones: List, zone_annotators: List, box_annotators: List, masks_annotators: List, label_annotators: List) -> None:
+
+    for zone_annotator in zone_annotators:
+        # visualize polygon for the zone
+        frame = zone_annotator.annotate(scene=frame)
+
+    if detections:
+        # uniquely track the objects
+        tracks = track_objects(frame, detections, tracker)
+
+        # annotate the frame with the detected persons within each zone
+        for zone_id, (zone, box_annotator, masks_annotator, label_annotator) in enumerate(
+                zip(zones, box_annotators, masks_annotators, label_annotators), start=1):
+
+            # get detections relevant only for the zone
+            mask = zone.trigger(detections=detections)
+            detections_filtered = detections[mask]
+            # visualize boxes around objects in the zone
+            frame = masks_annotator.annotate(scene=frame, detections=detections_filtered)
+            frame = box_annotator.annotate(scene=frame, detections=detections_filtered)
+            # count how many objects detected
+            det_count = len(detections_filtered)
+            # Add track ID annotations
+            label_annotator.annotate(scene=frame, detections=detections_filtered,
+                                     labels=[f"ID: {track.track_id if track.time_since_update == 0 else ' '}" for
+                                             track, in_zone in zip(tracks, mask) if in_zone])
+            # add the count to the list
+            queue_count[zone_id].append(det_count)
+            # calculate the mean number of customers in the queue
+            mean_customer_count = np.mean(queue_count[zone_id], dtype=np.int32)
+
+            # add alert text to the frame if necessary, flash every second
+            if mean_customer_count > object_limit and time.time() % 2 > 1:
+                utils.draw_text(frame, text=f"Intel employee required in zone {zone_id}!", point=(20, 20),
+                                font_color=(0, 0, 255))
+
+            # print an info about number of customers in the queue, ask for the more assistants if required
+            log.info(
+                f"Zone {zone_id}, avg {category} count: {mean_customer_count} {'Intel employee required!' if mean_customer_count > object_limit else ''}")
+
+
+def run(video_path: str, model_paths: Tuple[Path, Path], model_name: str = "", category: str = "person", zones_config_file: str = "",
         object_limit: int = 3, flip: bool = True, tracker_frames: int = 1800, colorful: bool = False, last_frames: int = 50) -> None:
-    # Export both FP16 & INT8 ONCE at startup
-    pt_path  = f"{model_name}.pt"
-    ir_fp16   = f"{model_name}_openvino_model"
-    ir_int8   = f"{model_name}_int8_openvino_model"
+    # Suppress Ultralytics verbose logs
+    log.getLogger("ultralytics").setLevel(log.ERROR)
+    log.basicConfig(level=log.INFO)
 
-    base = YOLO(pt_path)
-    # Export FP16 if missing
-    if not os.path.isdir(ir_fp16):
-        logging.info(f"Exporting FP16 IR to {ir_fp16}...")
-        base.export(format="openvino", half=True)
-    else:
-        logging.info(f"FP16 IR already exists at {ir_fp16}, skipping export.")
-    # Export INT8 if missing
-    if not os.path.isdir(ir_int8):
-        logging.info(f"Exporting INT8 IR to {ir_int8}...")
-        base.export(format="openvino", int8=True)
-    else:
-        logging.info(f"INT8 IR already exists at {ir_int8}, skipping export.")
+    model_mapping = {
+        "FP16": model_paths[0],
+        "INT8": model_paths[1],
+    }
 
-    # Start with FP16
-    current_precision = "FP16"
-    model = YOLO(ir_fp16, task="detect", verbose=False)
+    # Start with INT8
+    model_type = "INT8"
+    model = get_model(model_mapping[model_type], verbose=False)
 
     # Device setup
     devices_mapping = utils.available_devices()  # e.g. {"cpu":"Intel CPU", "gpu":"Intel GPU", ...}
     device_type = f"intel:{next(iter(devices_mapping.keys()))}"  # default to first available
 
-    # Zones & history
-    zones      = load_zones(zones_config_file)
-    rects      = [cv2.boundingRect(z) for z in zones]
-    history    = {i+1: deque(maxlen=last_frames) for i in range(len(zones))}
-    last_log   = {i+1: 0 for i in range(len(zones))}
-    times      = deque(maxlen=100)
-    cls_target = CATEGORIES.index(category)  # should be 0 for "person"
-
     # Video player
     if isinstance(video_path, str) and video_path.isnumeric():
         video_path = int(video_path)
     player = utils.VideoPlayer(video_path, size=(1920,1080), fps=60, flip=flip)
+
+    # get zones, and zone and box annotators for zones
+    zones, zone_annotators, box_annotators, masks_annotators, label_annotators = get_annotators(json_path=zones_config_file, resolution_wh=(player.width, player.height), colorful=colorful)
+    category_id = CATEGORIES.index(category)
+
+    # object counter
+    queue_count = defaultdict(lambda: deque(maxlen=last_frames))
+    # keep at most 100 last times
+    processing_times = deque(maxlen=100)
+
+    # Initialize the tracker with a higher max_age
+    tracker = DeepSort(max_age=tracker_frames, n_init=3)
 
     title = "Press ESC to Exit"
     cv2.namedWindow(title, cv2.WINDOW_GUI_NORMAL)
@@ -94,104 +197,54 @@ def run(video_path: str, model_name: str = "", category: str = "person", zones_c
             break
         # Get the results.
         frame = np.array(frame)
+        f_height, f_width = frame.shape[:2]
 
         # inference + timing
         start_time  = time.time()
-        res = model(frame, device=device_type, verbose=False)[0]
-        dt  = (time.time() - start_time) * 1000  # ms
-        times.append(dt)
+        result = model(frame, device=device_type, verbose=False)[0]
+        processing_times.append(time.time() - start_time)
 
-        # start from raw frame
-        annotated = frame.copy()
+        # convert to supervision detections
+        detections = sv.Detections.from_ultralytics(result)
 
-        # Draw each zone’s bounding box in red
-        for zx, zy, zw, zh in rects:
-            cv2.rectangle(annotated, (zx, zy), (zx + zw, zy + zh), (0, 0, 255), 2)
+        # draw results
+        draw_annotations(frame, detections, tracker, queue_count, object_limit, category, zones, zone_annotators, box_annotators, masks_annotators, label_annotators)
 
-        # Extract boxes & classes
-        boxes   = res.boxes.xyxy.cpu().numpy()
-        classes = res.boxes.cls.cpu().numpy().astype(int)
-        now     = time.time()
+        # Mean processing time [ms].
+        processing_time = np.mean(processing_times) * 1000
 
-        # Gather all person detections
-        person_indices = [(pid, box.astype(int))
-            for pid, (box, cls_id) in enumerate(zip(boxes, classes), start=1)
-            if cls_id == cls_target]
-
-        # Draw each person’s bounding box in green and their ID
-        for pid, (x1, y1, x2, y2) in person_indices:
-            cv2.rectangle(annotated, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            px, py = int((x1 + x2) / 2), y1 - 10
-            utils.draw_text(annotated, text=f"ID:{pid}", point=(px, py),font_color=(0, 255, 0))
-
-        # Draw total persons count
-        total_p = len(person_indices)
-        utils.draw_text(annotated, text=f"Total persons: {total_p}", point=(10, 10), font_color=(255,255,255))
-
-         # Per-zone average
-        for zid, ((zx, zy, zw, zh), hist) in enumerate(zip(rects, history.values()), start=1):
-            # Count current persons in zone
-            count = sum(1 for _, (x1, y1, x2, y2) in person_indices
-                if not (x2 < zx or x1 > zx+zw or y2 < zy or y1 > zy+zh))
-            # Record count history
-            if count or hist:
-                hist.append(count)
-            # Calculate the mean number of customers in the queue
-            mean_customer_count = int(np.mean(hist, dtype=np.int32)) if hist else 0
-
-            # Log and alert
-            if now - last_log[zid] >= 1:
-                alert_msg = ' Intel employee required!' if mean_customer_count > object_limit else ''
-                logging.info(f"Zone {zid}, avg {category} count: {mean_customer_count}{alert_msg}")
-                last_log[zid] = now
-            # Flash alert text every other second
-            if mean_customer_count > object_limit and now % 2 > 1:
-                utils.draw_text(annotated, text=f"Intel employee required in zone {zid}!", point=(20, 20), 
-                                font_color=(0, 0, 255))
-            # Draw zone average count
-            utils.draw_text(annotated, text=f"Zone {zid}: {mean_customer_count}", point=(zx + 5, zy + 20),
-                font_color=(0, 255, 255))
-
-        # Performance metrics
-        mean_t = sum(times)/len(times) if times else 0
-        fps    = 1000/mean_t if mean_t>0 else 0
-        h,w,_  = annotated.shape
-        utils.draw_text(annotated, text=f"Inference: {mean_t:.0f}ms ({fps:.1f} FPS)", point=(w//2, 10))
-        utils.draw_text(annotated, text=f"Model: {model_name} ({current_precision}) on {device_type}", point=(w//2, 50))
+        fps = 1000 / processing_time
+        utils.draw_text(frame, text=f"Inference time: {processing_time:.0f}ms ({fps:.1f} FPS)", point=(f_width * 3 // 5, 10))
+        utils.draw_text(frame, text=f"Currently running {model_name} ({model_type}) on {device_type}", point=(f_width * 3 // 5, 50))
 
         # Draw control panel & watermark
-        utils.draw_control_panel(annotated, devices_mapping)
-        utils.draw_ov_watermark(annotated)
+        utils.draw_control_panel(frame, devices_mapping)
+        utils.draw_ov_watermark(frame)
 
-        cv2.imshow(title, annotated)
+        cv2.imshow(title, frame)
         key = cv2.waitKey(1)
         # escape = 27 or 'q' to close the app
-        if key == 27 or key == ord('q'):
+        if key in (27, ord('q')):
             break
 
         # handle keypress for precision/device
         model_changed = False
 
-        if key == ord('f') and current_precision != "FP16":
-            current_precision = "FP16"
-            logging.info("Switched to FP16")
+        if key == ord('f'):
+            model_type = "FP16"
             model_changed = True
-        elif key == ord('i') and current_precision != "INT8":
-            current_precision = "INT8"
-            logging.info("Switched to INT8")
+        elif key == ord('i'):
+            model_type = "INT8"
             model_changed = True
-
-        for idx, dev in enumerate(devices_mapping.keys()):
-            if key == ord(str(idx+1)):
+        for i, dev in enumerate(devices_mapping.keys()):
+            if key == ord('1') + i:
                 device_type   = f"intel:{dev}"
-                logging.info(f"Switched to device {device_type}")
                 model_changed = True
 
         if model_changed:
             del model
-            folder = ir_fp16 if current_precision=="FP16" else ir_int8
-            model = YOLO(folder, task="detect", verbose=False)
-            times.clear()
+            model = get_model(model_mapping[model_type], verbose=False)
+            processing_times.clear()
 
     player.stop()
     cv2.destroyAllWindows()
@@ -203,6 +256,7 @@ if __name__ == '__main__':
     parser.add_argument("--model_name", type=str, default="yolo11n", help="Model version to be converted",
                         choices=["yolov8n", "yolov8s", "yolov8m", "yolov8l", "yolov8x", "yolov8n-seg", "yolov8s-seg", "yolov8m-seg", "yolov8l-seg", "yolov8x-seg",
                                  "yolo11n", "yolo11s", "yolo11m", "yolo11l", "yolo11x", "yolo11n-seg", "yolo11s-seg", "yolo11m-seg", "yolo11l-seg", "yolo11x-seg"])
+    parser.add_argument("--model_dir", type=str, default="model", help="Directory to place the model in")
     parser.add_argument('--category', type=str, default="person", choices=CATEGORIES, help="The category to detect (from COCO dataset)")
     parser.add_argument('--zones_config_file', type=str, default="zones.json", help="Path to the zone config file (json)")
     parser.add_argument('--object_limit', type=int, default=3, help="The maximum number of objects in the area")
@@ -211,5 +265,5 @@ if __name__ == '__main__':
     parser.add_argument('--tracker_frames', type=int, default=1800, help="Maximum number of missed frames for the tracker")
 
     args = parser.parse_args()
-    run(args.stream, args.model_name, args.category, args.zones_config_file, args.object_limit, args.tracker_frames, args.flip, args.colorful)
-    
+    model_paths = convert(args.model_name, Path(args.model_dir))
+    run(args.stream, model_paths, args.model_name, args.category, args.zones_config_file, args.object_limit, args.tracker_frames, args.flip, args.colorful)
