92470da9405e84f3e27772132f65910b7183a9b1
Raymond Lo <raymondlo84@hotmail.com>
2025-09-30 15:24:15 -0700

    automation scripts to run demos
    a set of automation scripts on Windows to avoid typos or errors for demo showing.
    
    We will make README updates once these are validated.

---
+++ ai_ref_kits/multimodal_ai_visual_generator/install.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Create virtual environment
+echo Creating virtual environment...
+python -m venv venv
+
+:: Activate virtual environment
+call venv\Scripts\activate.bat
+
+:: Upgrade pip
+echo Upgrading pip...
+python -m pip install --upgrade pip
+
+:: Install dependencies
+echo Installing dependencies...
+pip install -r requirements.txt
+
+:: Final success message
+echo.
+echo ========================================
+echo All requirements installed for Storyteller.
+echo You can now run the demo 
+echo 1. download_models.bat
+echo 2. run_backend.bat
+echo 3. run_frontend.bat
+echo ========================================
+pause
+exit
