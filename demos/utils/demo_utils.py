922ad604fc49f3911eb743dea68d05300ae98eab
Adrian Boguszewski <adrian.boguszewski@intel.com>
2025-12-18 14:01:58 +0100

    Remove nvidia from available devices - it doesn't work in OV

--- demos/utils/demo_utils.py
+++ demos/utils/demo_utils.py
@@ -198,8 +198,9 @@ def available_devices(exclude: list | tuple | None = None) -> Dict[str, str]:
 
     core = ov.Core()
     for device in core.available_devices:
-        if device not in exclude_devices:
-            device_mapping[device] = core.get_property(device, "FULL_DEVICE_NAME")
+        device_name = core.get_property(device, "FULL_DEVICE_NAME")
+        if "nvidia" not in device_name.lower() and device not in exclude_devices:
+            device_mapping[device] = device_name
 
     return device_mapping
 
