c13d49a13ce5256c0f825b9ef8a0e9b7b0945577
Ezequiel Lanza <ezequiel.lanza@gmail.com>
2026-01-12 16:45:23 -0500

    UI updates

--- ai_ref_kits/agentic_multimodal_travel_planer/start_ui.py
+++ ai_ref_kits/agentic_multimodal_travel_planer/start_ui.py
@@ -227,13 +227,14 @@ class TravelRouterClient:
 # Example prompts for the UI
 EXAMPLES = [
     "Which city is shown in this image?",
-    "Give me flights from New York to Paris for March 1st to March 10th",
-    "Give me hotels in Paris for March 1st to March 10th for 2 guests",
+    "Give me flights from Milan to Rome for March 1st to March 10th",
+    "Give me hotels in Milan for March 1st to March 10th for 2 guests",
     (
-        "Give me flights from New York to Paris for March 1st to March 10th "
+        "Give me flights from Milan to Rome for March 1st to March 10th "
         "in business class"
     ),
 ]
+
 
 # Global state variables
 chatbox_msg = []
