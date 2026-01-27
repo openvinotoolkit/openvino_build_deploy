f25192af38efa1caf71aa7040d6d6372ae0e7c0e
Adrian Boguszewski <adrian.boguszewski@intel.com>
2026-01-27 12:28:10 +0100

    Delete workshops directory

--- workshops/CVPR2025/src/main_app.py
+++
@@ -1,5 +0,0 @@
-from mcp_clients.working_video_search_client import main
-import asyncio
-
-if __name__ == "__main__":
-    asyncio.run(main())
--- workshops/CVPR2025/src/main_search_server.py
+++
@@ -1,7 +0,0 @@
-from mcp_servers.bridgetower_search.video_processing_server import mcp
-# import asyncio
-
-if __name__ == "__main__":
-    # asyncio.run(main())
-    # mcp.run(transport="stdio")
-    mcp.run(transport="sse")
